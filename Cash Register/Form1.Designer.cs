edb1eb9517c70fe833ae2a0f38e92ed942f63daa
Kiril Covaliov <kiricova735@outlook.com>
2021-02-10 14:37:16 -0500

    Add project files.

---
+++ Cash Register/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace Cash_Register
+{
+    partial class backround
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.topTitleLabel = new System.Windows.Forms.Label();
+            this.computersLabel = new System.Windows.Forms.Label();
+            this.televisionsLabel = new System.Windows.Forms.Label();
+            this.phoneLabel = new System.Windows.Forms.Label();
+            this.computerText = new System.Windows.Forms.TextBox();
+            this.tvText = new System.Windows.Forms.TextBox();
+            this.phoneText = new System.Windows.Forms.TextBox();
+            this.totalButton = new System.Windows.Forms.Button();
+            this.subtotalLabel = new System.Windows.Forms.Label();
+            this.subtotalOutput = new System.Windows.Forms.Label();
+            this.taxLabel = new System.Windows.Forms.Label();
+            this.taxOutput = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.totalOutput = new System.Windows.Forms.Label();
+            this.horizontalDivider = new System.Windows.Forms.Label();
+            this.tenderedLabel = new System.Windows.Forms.Label();
+            this.tenderedText = new System.Windows.Forms.TextBox();
+            this.changeButton = new System.Windows.Forms.Button();
+            this.changeLabel = new System.Windows.Forms.Label();
+            this.changeOutput = new System.Windows.Forms.Label();
+            this.receiptButton = new System.Windows.Forms.Button();
+            this.verticalDivider = new System.Windows.Forms.Label();
+            this.receiptOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // topTitleLabel
+            // 
+            this.topTitleLabel.BackColor = System.Drawing.Color.Blue;
+            this.topTitleLabel.Font = new System.Drawing.Font("Mixage Bk BT", 23.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.topTitleLabel.ForeColor = System.Drawing.Color.Yellow;
+            this.topTitleLabel.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.topTitleLabel.Location = new System.Drawing.Point(-1, 0);
+            this.topTitleLabel.Name = "topTitleLabel";
+            this.topTitleLabel.Size = new System.Drawing.Size(802, 53);
+            this.topTitleLabel.TabIndex = 0;
+            this.topTitleLabel.Text = "BestBought";
+            // 
+            // computersLabel
+            // 
+            this.computersLabel.AutoSize = true;
+            this.computersLabel.Font = new System.Drawing.Font("Mirarae BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.computersLabel.Location = new System.Drawing.Point(3, 69);
+            this.computersLabel.Name = "computersLabel";
+            this.computersLabel.Size = new System.Drawing.Size(260, 18);
+            this.computersLabel.TabIndex = 1;
+            this.computersLabel.Text = "Number of Computers Purchased:";
+            // 
+            // televisionsLabel
+            // 
+            this.televisionsLabel.AutoSize = true;
+            this.televisionsLabel.Font = new System.Drawing.Font("Mirarae BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.televisionsLabel.Location = new System.Drawing.Point(3, 103);
+            this.televisionsLabel.Name = "televisionsLabel";
+            this.televisionsLabel.Size = new System.Drawing.Size(262, 18);
+            this.televisionsLabel.TabIndex = 2;
+            this.televisionsLabel.Text = "Number of Televisions Purchased:";
+            // 
+            // phoneLabel
+            // 
+            this.phoneLabel.AutoSize = true;
+            this.phoneLabel.Font = new System.Drawing.Font("Mirarae BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.phoneLabel.Location = new System.Drawing.Point(3, 138);
+            this.phoneLabel.Name = "phoneLabel";
+            this.phoneLabel.Size = new System.Drawing.Size(278, 18);
+            this.phoneLabel.TabIndex = 3;
+            this.phoneLabel.Text = "Number of SmartPhones Purchased:";
+            // 
+            // computerText
+            // 
+            this.computerText.Location = new System.Drawing.Point(294, 65);
+            this.computerText.Name = "computerText";
+            this.computerText.Size = new System.Drawing.Size(61, 22);
+            this.computerText.TabIndex = 4;
+            // 
+            // tvText
+            // 
+            this.tvText.Location = new System.Drawing.Point(294, 103);
+            this.tvText.Name = "tvText";
+            this.tvText.Size = new System.Drawing.Size(61, 22);
+            this.tvText.TabIndex = 5;
+            // 
+            // phoneText
+            // 
+            this.phoneText.Location = new System.Drawing.Point(294, 138);
+            this.phoneText.Name = "phoneText";
+            this.phoneText.Size = new System.Drawing.Size(61, 22);
+            this.phoneText.TabIndex = 6;
+            // 
+            // totalButton
+            // 
+            this.totalButton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.totalButton.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.totalButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGray;
+            this.totalButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.totalButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalButton.Location = new System.Drawing.Point(92, 176);
+            this.totalButton.Name = "totalButton";
+            this.totalButton.Size = new System.Drawing.Size(136, 23);
+            this.totalButton.TabIndex = 7;
+            this.totalButton.Text = "TOTAL";
+            this.totalButton.UseVisualStyleBackColor = false;
+            // 
+            // subtotalLabel
+            // 
+            this.subtotalLabel.AutoSize = true;
+            this.subtotalLabel.Font = new System.Drawing.Font("Mirarae BT", 12F);
+            this.subtotalLabel.Location = new System.Drawing.Point(34, 212);
+            this.subtotalLabel.Name = "subtotalLabel";
+            this.subtotalLabel.Size = new System.Drawing.Size(85, 18);
+            this.subtotalLabel.TabIndex = 8;
+            this.subtotalLabel.Text = "Sub Total:";
+            // 
+            // subtotalOutput
+            // 
+            this.subtotalOutput.Font = new System.Drawing.Font("Nirmala UI", 12F);
+            this.subtotalOutput.Location = new System.Drawing.Point(138, 209);
+            this.subtotalOutput.Name = "subtotalOutput";
+            this.subtotalOutput.Size = new System.Drawing.Size(58, 21);
+            this.subtotalOutput.TabIndex = 9;
+            this.subtotalOutput.Text = "$00.00";
+            // 
+            // taxLabel
+            // 
+            this.taxLabel.AutoSize = true;
+            this.taxLabel.Font = new System.Drawing.Font("Mirarae BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taxLabel.Location = new System.Drawing.Point(79, 233);
+            this.taxLabel.Name = "taxLabel";
+            this.taxLabel.Size = new System.Drawing.Size(40, 18);
+            this.taxLabel.TabIndex = 10;
+            this.taxLabel.Text = "Tax:";
+            // 
+            // taxOutput
+            // 
+            this.taxOutput.AutoSize = true;
+            this.taxOutput.Font = new System.Drawing.Font("Nirmala UI", 12F);
+            this.taxOutput.Location = new System.Drawing.Point(138, 233);
+            this.taxOutput.Name = "taxOutput";
+            this.taxOutput.Size = new System.Drawing.Size(58, 21);
+            this.taxOutput.TabIndex = 11;
+            this.taxOutput.Text = "$00.00";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Font = new System.Drawing.Font("Mirarae BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalLabel.Location = new System.Drawing.Point(67, 254);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(52, 18);
+            this.totalLabel.TabIndex = 13;
+            this.totalLabel.Text = "Total:";
+            // 
+            // totalOutput
+            // 
+            this.totalOutput.AutoSize = true;
+            this.totalOutput.Font = new System.Drawing.Font("Nirmala UI", 12F);
+            this.totalOutput.Location = new System.Drawing.Point(138, 254);
+            this.totalOutput.Name = "totalOutput";
+            this.totalOutput.Size = new System.Drawing.Size(58, 21);
+            this.totalOutput.TabIndex = 14;
+            this.totalOutput.Text = "$00.00";
+            // 
+            // horizontalDivider
+            // 
+            this.horizontalDivider.BackColor = System.Drawing.Color.Blue;
+            this.horizontalDivider.Location = new System.Drawing.Point(0, 275);
+            this.horizontalDivider.Name = "horizontalDivider";
+            this.horizontalDivider.Size = new System.Drawing.Size(376, 10);
+            this.horizontalDivider.TabIndex = 15;
+            // 
+            // tenderedLabel
+            // 
+            this.tenderedLabel.AutoSize = true;
+            this.tenderedLabel.Font = new System.Drawing.Font("Nirmala UI", 12F);
+            this.tenderedLabel.Location = new System.Drawing.Point(88, 305);
+            this.tenderedLabel.Name = "tenderedLabel";
+            this.tenderedLabel.Size = new System.Drawing.Size(143, 21);
+            this.tenderedLabel.TabIndex = 16;
+            this.tenderedLabel.Text = "Currency Tendered:";
+            this.tenderedLabel.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // tenderedText
+            // 
+            this.tenderedText.Location = new System.Drawing.Point(255, 304);
+            this.tenderedText.Name = "tenderedText";
+            this.tenderedText.Size = new System.Drawing.Size(100, 22);
+            this.tenderedText.TabIndex = 17;
+            // 
+            // changeButton
+            // 
+            this.changeButton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.changeButton.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.changeButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGray;
+            this.changeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.changeButton.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeButton.Location = new System.Drawing.Point(92, 342);
+            this.changeButton.Name = "changeButton";
+            this.changeButton.Size = new System.Drawing.Size(136, 23);
+            this.changeButton.TabIndex = 18;
+            this.changeButton.Text = "CALCULATE CHANGE";
+            this.changeButton.UseVisualStyleBackColor = false;
+            // 
+            // changeLabel
+            // 
+            this.changeLabel.AutoSize = true;
+            this.changeLabel.Font = new System.Drawing.Font("Nirmala UI", 12F);
+            this.changeLabel.Location = new System.Drawing.Point(88, 380);
+            this.changeLabel.Name = "changeLabel";
+            this.changeLabel.Size = new System.Drawing.Size(66, 21);
+            this.changeLabel.TabIndex = 19;
+            this.changeLabel.Text = "Change:";
+            // 
+            // changeOutput
+            // 
+            this.changeOutput.AutoSize = true;
+            this.changeOutput.Font = new System.Drawing.Font("Nirmala UI", 12F);
+            this.changeOutput.Location = new System.Drawing.Point(170, 380);
+            this.changeOutput.Name = "changeOutput";
+            this.changeOutput.Size = new System.Drawing.Size(58, 21);
+            this.changeOutput.TabIndex = 20;
+            this.changeOutput.Text = "$00.00";
+            // 
+            // receiptButton
+            // 
+            this.receiptButton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.receiptButton.FlatAppearance.BorderColor = System.Drawing.Color.DarkGray;
+            this.receiptButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.receiptButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.receiptButton.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.receiptButton.Location = new System.Drawing.Point(92, 417);
+            this.receiptButton.Name = "receiptButton";
+            this.receiptButton.Size = new System.Drawing.Size(136, 23);
+            this.receiptButton.TabIndex = 21;
+            this.receiptButton.Text = "PRINT RECEIPT";
+            this.receiptButton.UseVisualStyleBackColor = false;
+            // 
+            // verticalDivider
+            // 
+            this.verticalDivider.BackColor = System.Drawing.Color.Blue;
+            this.verticalDivider.Location = new System.Drawing.Point(373, 53);
+            this.verticalDivider.Name = "verticalDivider";
+            this.verticalDivider.Size = new System.Drawing.Size(10, 414);
+            this.verticalDivider.TabIndex = 22;
+            // 
+            // receiptOutput
+            // 
+            this.receiptOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.receiptOutput.Location = new System.Drawing.Point(427, 69);
+            this.receiptOutput.Name = "receiptOutput";
+            this.receiptOutput.Size = new System.Drawing.Size(296, 371);
+            this.receiptOutput.TabIndex = 23;
+            // 
+            // backround
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(765, 462);
+            this.Controls.Add(this.receiptOutput);
+            this.Controls.Add(this.verticalDivider);
+            this.Controls.Add(this.receiptButton);
+            this.Controls.Add(this.changeOutput);
+            this.Controls.Add(this.changeLabel);
+            this.Controls.Add(this.changeButton);
+            this.Controls.Add(this.tenderedText);
+            this.Controls.Add(this.tenderedLabel);
+            this.Controls.Add(this.horizontalDivider);
+            this.Controls.Add(this.totalOutput);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.taxOutput);
+            this.Controls.Add(this.taxLabel);
+            this.Controls.Add(this.subtotalOutput);
+            this.Controls.Add(this.subtotalLabel);
+            this.Controls.Add(this.totalButton);
+            this.Controls.Add(this.phoneText);
+            this.Controls.Add(this.tvText);
+            this.Controls.Add(this.computerText);
+            this.Controls.Add(this.phoneLabel);
+            this.Controls.Add(this.televisionsLabel);
+            this.Controls.Add(this.computersLabel);
+            this.Controls.Add(this.topTitleLabel);
+            this.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "backround";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label topTitleLabel;
+        private System.Windows.Forms.Label computersLabel;
+        private System.Windows.Forms.Label televisionsLabel;
+        private System.Windows.Forms.Label phoneLabel;
+        private System.Windows.Forms.TextBox computerText;
+        private System.Windows.Forms.TextBox tvText;
+        private System.Windows.Forms.TextBox phoneText;
+        private System.Windows.Forms.Button totalButton;
+        private System.Windows.Forms.Label subtotalLabel;
+        private System.Windows.Forms.Label subtotalOutput;
+        private System.Windows.Forms.Label taxLabel;
+        private System.Windows.Forms.Label taxOutput;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label totalOutput;
+        private System.Windows.Forms.Label horizontalDivider;
+        private System.Windows.Forms.Label tenderedLabel;
+        private System.Windows.Forms.TextBox tenderedText;
+        private System.Windows.Forms.Button changeButton;
+        private System.Windows.Forms.Label changeLabel;
+        private System.Windows.Forms.Label changeOutput;
+        private System.Windows.Forms.Button receiptButton;
+        private System.Windows.Forms.Label verticalDivider;
+        private System.Windows.Forms.Label receiptOutput;
+    }
+}
+
---
+++ Cash Register/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cash_Register
+{
+    public partial class backround : Form
+    {
+        int computer = 750;
+        int television = 500;
+        int smartphone = 635;
+        double tax = 0.13;
+        double subtotal;
+        double totalcost;
+        double change;
+        double tendered;
+        
+        
+
+        public backround()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
